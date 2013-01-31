1a57e9a9967334d54449b0aabcef59976da34fc5
Nikki Stevens <nikkilstevens@gmail.com>
2013-01-31 15:11:26 -0500

    No null returns, plus new stats

--- barista/app/controllers/orders_controller.rb
+++ barista/app/controllers/orders_controller.rb
@@ -15,6 +15,12 @@ class OrdersController < ApplicationController
   def show
     @order = Order.find(params[:id])
 
+    # if the order is incomplete, we need to return the order's
+    # place in the queue and the approximate wait time.
+    # adding one to this so it's not zero based, for the people.
+     @order[:queue_place] = Order.where('fulfilled IS NULL AND placed < ?', @order.placed).count + 1
+     @order[:wait_time] = StoreConfig.find('avg_wait_time').value
+
     respond_to do |format|
       format.html # show.html.erb
       format.json { render json: @order }
@@ -44,6 +50,9 @@ class OrdersController < ApplicationController
 
     respond_to do |format|
       if @order.save
+        # brb WET code.
+        @order[:queue_place] = Order.where('fulfilled IS NULL AND placed < ?', @order.placed).count + 1
+        @order[:wait_time] = StoreConfig.find('avg_wait_time').value
         format.html { redirect_to @order, notice: 'Order was successfully created.' }
         format.json { render json: @order, status: :created, location: @order }
       else
@@ -98,8 +107,6 @@ class OrdersController < ApplicationController
     end
 
   end
-
-
 
   # GET /queue
   # GET /queue.json
